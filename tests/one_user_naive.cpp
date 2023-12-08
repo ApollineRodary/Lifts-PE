a0de96aebb1e793b4dd3074fcaac3e61641bb6eb
Apolline Rodary <apollinerodary@gmail.com>
2023-12-08 16:12:40 +0100

    Update naive algorithm test

--- tests/one_user_naive.cpp
+++ tests/one_user_naive.cpp
@@ -17,31 +17,28 @@ public:
     void call(int floor, int time, string command) override {
         debugStream("TestElevatorSystem::call") << '!' << time << '?' << command << '!' << floor << endl;
         for (auto elevator: getElevators())
-            elevator->moveToFloor(floor);
+            elevator->moveToFloor(floor, time);
     }
 };
 
 class TestElevator: public Elevator {
 public:
-    TestElevator(ElevatorSystem& system, int floor): Elevator(system, floor) {};
+    TestElevator(ElevatorSystem& system, int floor, int capacity): Elevator(system, floor, capacity) {};
 
-    void requestFloor(int floor) override {
-        debugStream("TestElevator::requestFloor") << '!' << floor << endl;
-        moveToFloor(floor);
+    void requestFloor(int floor, int time) override {
+        debugStream("TestElevator::requestFloor") << '!' << time << '?' << " " << '!' << floor << endl;
+        moveToFloor(floor, time);
     }
 };
 
 int main() {
     // One single elevator, starting at floor 2
     TestElevatorSystem system(0, 4);
-    TestElevator e(system, 2);
+    TestElevator e(system, 2, 3);
     
     // One single user, going from floor 0 to floor 3
-    User user(system);
-    Goal g;
-    g.source_floor = 0;
-    g.target_floor = 3;
-    g.time = 5;
+    User user(system, 0, 1);
+    Goal g(0, 3, 5);
     user.addGoal(g);
 
     vector<User*> users;
