d99ed1e7dc9722ad575577192a0452877cb4a0e2
QuiCouRt <151925055+QuiCouRt@users.noreply.github.com>
2023-12-10 20:50:57 +0100

    Update model.py

--- model.py
+++ model.py
@@ -53,7 +53,7 @@ def G(m,l,i):
         print("ERROR PROBA")
     return sum
 
-# <<<<<<Expectation function>>>>>>
+# Expectation function
 #This function allows us to calculate the expectation 
 #of travel time of the elevator for i passangers
 def exp(i):
@@ -68,16 +68,24 @@ def exp(i):
     return sum
 
 
-#<<<<<<Graph>>>>>>
-#This function create the graph of the expectation as a function of  lambda
-mu = 1.0/exp(c)
+#Graph Function
+#This function create the graph of the expectation as a function of i 
+mu = 6.0/exp(c)
 x=np.arange(0, 2.2, 0.2)
-lam=x/6.0
+lam=x
 y=1.0/(mu-lam)
 plt.plot(x, y)
-plt.title('The graph of the expectation waiting time as a function of $\lambda$')
+mu_prime = 1.0/exp(c)
+x_prime=np.arange(0, 2.2, 0.2)
+lam_prime=x/6
+y_prime=1.0/(mu_prime-lam_prime)
+plt.plot(x,y_prime)
+#plt.title('The graph of the expectation as a function of i')
 plt.xlabel('Value of $\lambda$ (passengers/minutes)')
 plt.ylabel('The expectation of waiting time (in minutes)')
 plt.show()
 
+l=0.2
+print("exp time model_2", 1.0/(mu-l))
+print("exp time model_1", 1.0/((mu/6)-l/6))
 #print("Expectation of travel time of the elevator for i passangers",exp(6))
