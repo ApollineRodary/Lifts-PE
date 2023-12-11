4ef341c48b146b774b140437ccedbd790d0f1564
Apolline Rodary <apollinerodary@gmail.com>
2023-12-11 15:00:46 +0100

    Rename and clean up Python graphing script

---
+++ graphs.py
@@ -0,0 +1,97 @@
+from math import *
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+max_floor = 4
+capacity = 6
+t_floor = 0.1
+t_stop = 0.3
+
+
+def partition_aux(l, bins):
+    if len(l)==1 or bins==1:
+        yield [l]
+    elif len(l)>1 and bins>1:
+        for i in range(1, len(l)):
+            for part in partition_aux(l[i:], bins-1):
+                if len(part) + 1 == bins:
+                    yield [l[:i]] + part
+
+def partition(m, i):
+    """Compute all ways of summing m positive integers to have i
+    Used to compute all possible distributions of i passengers on m floors"""
+    l = []
+    for pa in partition_aux(range(1, i+1), m):
+        li_2 = []
+        for e in pa :
+            li_2.append(len(e))
+        l.append(li_2)
+    return l
+
+def G(m, l, i):
+    """Compute the probability of making m stops on our way to floor l"""
+    sum = 0
+    li = partition(m, i)
+    for tab in li :
+        prod = 1
+        sum_g = 0
+        for e in range(len(tab)):
+            prod *= comb(i-sum_g, tab[e])
+            sum_g += tab[e]
+        sum += prod
+    sum *= comb(l-1, m-1)
+    sum /= pow(max_floor, i)
+
+    assert(sum <= 1)
+    return sum
+
+def expected_waiting_time(i):
+    """Compute expected travel time of the elevator for i passengers"""
+    sum = 0
+    sum_m = 0
+    for l in range(1, max_floor+1):
+        for m in range(1, l+1):
+            p = ((m+1)*t_stop + 2*l*t_floor)
+            g = G(m, l, i)
+            sum += p*g
+            sum_m += m*g
+    return sum
+ 
+# Graph expected waiting time as a function of average arrival rate
+
+x=np.arange(0, 2.2, 0.2)
+
+WHICH_GRAPH = 1
+
+if WHICH_GRAPH == 1:
+    # Thibault's
+    mu = 6/expected_waiting_time(capacity)
+    lam = x
+    y = 1.0/(mu-lam) + 0.6
+
+elif WHICH_GRAPH == 2:
+    # Isaline's
+    mu = 1/expected_waiting_time(capacity)
+    lam = x/6
+    y = 1.0/(mu - lam)
+
+plt.plot(x, y, label="$1/(\mu - \lambda)$")
+
+# Graph simulation results as a function of average arrival rate
+
+with open("results/one_src_multiple_target.json", "r") as json_file:
+    simulation_results = json.load(json_file)["results"]
+
+mean = sum([np.array(d["means"]) for d in simulation_results]) / len(simulation_results)
+
+plt.plot(simulation_results[0]["lambdas"], mean, label="Monod simulation")
+plt.legend()
+
+plt.xlabel('$\lambda$ (arrivals/min)')
+plt.ylabel('Expected waiting time (min)')
+plt.show()
+
+""" l=0.2
+print("exp time model_2", 1.0/(mu-l))
+print("exp time model_1", 1.0/((mu/6)-l/6)) """
